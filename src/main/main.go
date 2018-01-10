073d35911bc9916c42ae9e1b59d384b3961df4ba
Laucr <leo.lau.inami@gmail.com>
2018-01-10 23:56:25 +0800

    html render by "http://www.motion-express.com/blog/serve-static-files-in-gin"

--- src/main/main.go
+++ src/main/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"controller"
+	"net/http"
 )
 
 func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("src/main/*.html")
 	router.POST("/register/submit", controller.Register)
 	router.POST("/login/submit", controller.PostLogin)
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "index",
+			"body": "hello gingo"})
+	})
 	router.Run(":51234")
 }
 
